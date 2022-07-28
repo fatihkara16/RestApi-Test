afa1a4a5f7ce75fabdf877e31ff4eae76ba29789
fatihkara16 <fatihkara1699@gmail.com>
2022-07-28 17:03:59 +0300

    proje yüklendi

---
+++ RestApi/Data/ICommanderRepo.cs
@@ -0,0 +1,14 @@
+﻿using RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Data
+{    
+    public interface ICommanderRepo
+    {
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int id);
+    }
+}
